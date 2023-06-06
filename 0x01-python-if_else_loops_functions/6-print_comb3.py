f4eb3918af2124e9be7037bb45f58f8dece69e01
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-06-06 23:11:02 +0000

    Prints Combinations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for x in range(1, 9):
-    for y in range(x + 2, 11):
+for x in range(1, 6):
+    for y in range(x + 2, 7):
         print("{:d}{:d}".format(x, y), end=', ')
 print("{:d}{:d}".format(x + 1, y))
