cf345d107315d65299b734df29cff0b39dbcc740
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-06-09 01:44:03 +0000

    Uppercase Alphabet

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-
 import string
-
-
-def print_ascii_uppercase():
-    print(string.ascii_uppercase)
-
-
-if __name__ == "__main__":
-    print_ascii_uppercase()
+print(string.ascii_uppercase)
