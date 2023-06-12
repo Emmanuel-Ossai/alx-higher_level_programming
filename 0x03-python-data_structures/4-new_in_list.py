e38abbad9a2f162c1332dd3a673905d05d5ade7e
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-06-12 22:07:37 +0000

    Replace element in a list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    if 0 <= idx < len(my_list):
+        copy = my_list.copy()
+        copy[idx] = element
+        return copy
+    return my_list
