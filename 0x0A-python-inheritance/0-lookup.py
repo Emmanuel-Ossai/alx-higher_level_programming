871b22435a7a838544c01cd553201509a06acab7
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-07-10 23:51:14 +0000

    Lookup function

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,23 @@
+def lookup(obj):
+
+    """
+    Returns a list of available attributes and methods of an object.
+
+    Args:
+        obj (object): The object to inspect.
+
+    Returns:
+        list: A list of available attributes and methods.
+
+    Examples:
+        >>> obj = SomeClass()
+        >>> attributes = lookup(obj)
+        >>> print(attributes)
+        ['attribute1', 'method1', 'method2']
+
+    """
+    attributes = []
+    for attr in dir(obj):
+        if not attr.startswith('__'):
+            attributes.append(attr)
+    return attributes
