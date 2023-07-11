bac589355707b99e170565d649e3a430ab48059c
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-07-11 01:49:25 +0000

    Geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+The class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Base class for geometry objects.
+    """
+
+    pass
+
+
+if __name__ == "__main__":
+    """
+    This module defines the BaseGeometry class.
+
+    Usage:
+        from module_name import BaseGeometry
+        geometry = BaseGeometry()
+    """
+    pass
