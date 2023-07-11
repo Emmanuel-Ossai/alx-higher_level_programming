37a89991ac123de1ac72db9941e9ba0854fd1764
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-07-11 00:16:39 +0000

    My list class

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+This python class contains the MyList class
+"""
+
+
+class MyList(list):
+    """
+    A custom list class that inherits from the built-in list class.
+
+    This class provides an additional method for printing the list
+
+    in sorted order.
+
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list in sorted (ascending) order.
+
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
