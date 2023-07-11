fe650e5779e52063acd79a69c464bd29e9524359
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-07-11 02:27:16 +0000

    Square 2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Contains a Rectangle subclass Square.
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """This shows a square."""
+
+    def __init__(self, size):
+        """Starting a new square.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
