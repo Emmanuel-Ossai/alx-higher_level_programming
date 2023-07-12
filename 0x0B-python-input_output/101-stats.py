60d935454c575c112e7a76e9d2ac10f74246b823
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-07-12 03:03:48 +0000

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -11,23 +11,24 @@ import sys
 file_size = 0
 status_tally = {"200": 0, "301": 0, "400": 0, "401": 0,
                 "403": 0, "404": 0, "405": 0, "500": 0}
-i = 0
+
+x = 0
 try:
     for line in sys.stdin:
         tokens = line.split()
         if len(tokens) >= 2:
-            a = i
+            t = x
             if tokens[-2] in status_tally:
                 status_tally[tokens[-2]] += 1
-                i += 1
+                x += 1
             try:
                 file_size += int(tokens[-1])
-                if a == i:
-                    i += 1
+                if t == x:
+                    x += 1
             except FileNotFoundError:
-                if a == i:
+                if t == x:
                     continue
-        if i % 10 == 0:
+        if x % 10 == 0:
             print("File size: {:d}".format(file_size))
             for key, value in sorted(status_tally.items()):
                 if value:
