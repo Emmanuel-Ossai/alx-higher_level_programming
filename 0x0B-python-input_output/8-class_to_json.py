a4bd27d66f331c9f2dfbef4e506afd26eeed8a0b
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2023-07-12 02:21:43 +0000

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -9,23 +9,7 @@ with simple data structure
 def class_to_json(obj):
     """
     Return the dictionary description of an object with a simple data
-    structure for JSON serialization.
+    structure for JSON serialization
 
     """
-    if not isinstance(obj, type):
-        # Checks if obj is not a Class instance, raise a TypeError
-        raise TypeError("obj must be an instance of a Class")
-
-    # Checks and get the attributes of the object
-    attributes = obj.__dict__
-
-    # Create an empty dictionary to store the serialized attributes
-    serialized = {}
-
-    # Iterate over each attribute
-    for attr, value in attributes.items():
-        # Check the type of the attribute and serialize accordingly
-        if isinstance(value, (list, dict, str, int, bool)):
-            serialized[attr] = value
-
-    return serialized
+    return obj.__dict__
