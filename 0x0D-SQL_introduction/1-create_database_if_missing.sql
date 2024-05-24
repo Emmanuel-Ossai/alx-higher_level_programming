a33440f1607977f7821773dcf953817dcd426cdd
Emmanuel Ossai <emmanuelbestossai@gmail.com>
2024-05-24 05:26:55 +0000

    SQL Tasks

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- This is a script that creates a database in my MySQL server
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- This is a script that deletes a database in my MySQL server
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
